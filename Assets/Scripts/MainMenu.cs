789a1bd7cb834ce8f7a0c1b5634b6c369a132524
simjinyi <38304422+simjinyi@users.noreply.github.com>
2020-06-17 17:43:25 +0800

    Update

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public Button btnStart;
+    public Button btnInstruction;
+    public Button btnCredit;
+    public Button btnSettings;
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void LoadInstruction()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
+    }
+
+    public void LoadCredit()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+
+
+    public void LoadSettings()
+    {
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 4);
+    }
+
+    public void Start()
+    {
+        btnStart.onClick.AddListener(StartGame);
+        btnInstruction.onClick.AddListener(LoadInstruction);
+        btnCredit.onClick.AddListener(LoadCredit);
+        btnSettings.onClick.AddListener(LoadSettings);
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("Quit Game!");
+        Application.Quit();
+    }
+}
