4177224635738e2b91901da6072083e7b9883758
melty0406 <melaniecheah46@gmail.com>
2020-07-22 01:18:25 +0800

    Game Over Button

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameOver : MonoBehaviour
+{
+    public Button ReplayGameOverButton;
+    public Button QuitGameOverButton;
+
+    public void ReplayGameOver()
+    {
+        Debug.Log("Replay");
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void QuitGameOver()
+    {
+        SceneManager.LoadScene("MainMenuScene");
+    }
+
+    void Start()
+    {
+
+        ReplayGameOverButton.onClick.AddListener(ReplayGameOver);
+        QuitGameOverButton.onClick.AddListener(QuitGameOver);
+
+
+
+    }
+}
