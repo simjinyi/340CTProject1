b4c2c7f2203cee1b07389ca83393b852622dc07a
Jessong99 <41329895+Jessong99@users.noreply.github.com>
2020-07-21 20:03:28 +0800

    DropDown Update

---
+++ Assets/Scripts/DropDownValue.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+//[RequireComponent(typeof(DropdownAge))]
+public class SaveDropDownValue : MonoBehaviour
+{
+    Dropdown.OptionData m_NewData, m_NewData2, m_NewData3;
+    List<Dropdown.OptionData> m_Messages = new List<Dropdown.OptionData>();
+
+    Dropdown DropdownAge;
+    string m_MyString;
+    int m_Index;
+
+    const string PrefName = "Age";
+
+    void Awake()
+    {
+        DropdownAge = transform.GetComponent<Dropdown>();
+        DropdownAge.onValueChanged.AddListener(delegate
+        {
+            PlayerPrefs.SetInt(PrefName, DropdownAge.value);
+            PlayerPrefs.Save();
+        });
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Fetch the Dropdown GameObject the script is attached to
+        DropdownAge = transform.GetComponent<Dropdown>();
+        //Clear the old options of the Dropdown menu
+        DropdownAge.ClearOptions();
+
+        /*
+        m_NewData = new Dropdown.OptionData();
+        m_NewData.text = "6 - 8";
+        m_Messages.Add(m_NewData);
+
+        //Create a new option for the Dropdown menu which reads "Option 2" and add to messages List
+        m_NewData2 = new Dropdown.OptionData();
+        m_NewData2.text = "9 - 10";
+        m_Messages.Add(m_NewData2);
+
+        //Create a new option for the Dropdown menu which reads "Option 3" and add to messages List
+        m_NewData3 = new Dropdown.OptionData();
+        m_NewData3.text = "11 - 12";
+        m_Messages.Add(m_NewData2);
+        
+
+        //Take each entry in the message List
+        foreach (Dropdown.OptionData message in m_Messages)
+        {
+            //Add each entry to the Dropdown
+            DropdownAge.options.Add(message);
+            //Make the index equal to the total number of entries
+            m_Index = m_Messages.Count - 1;
+        }
+        */
+
+        DropdownAge.value = PlayerPrefs.GetInt(PrefName, 0);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    //
+}
